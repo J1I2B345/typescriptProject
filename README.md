b41f1c38ed7c09adf6c20a2acc2e3b989502b91d
J1I2B345 <juan.bevilacqua@outlook.com>
2022-09-09 12:50:44 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1> First approach to TypeScript
+
+The objective of this repository, following SoyHenry Workshop, is to get the basics of TypeScript
+
+<h1> Technologies
+
+TypeScript and React
--- typescriptproject/src/App.tsx
+++ typescriptproject/src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//si le vamos a pasar props, tienen que ir acá ... si no va a tirar error.
+// en donde le pasamos las props solo las pasamos.
+interface AppProps {
+	title: string;
+}
+// destructuring
+// sin destructuring sería App(props: AppProps)
+//                ^^
+//                ||              /// el tipo de archivo que devuelve App por si esta en modo estricto
+//                ||                    ^^
+//                ||                    ||
+// functional
+// function App({ title }: AppProps): JSX.Element {
+// 	const [counter, setCounter] = useState(0);
+// 	return (
+// 		<div className="App">
+// 			<header className="App-header">
+// 				<h1> Workshop {title} </h1>
+//        <h1>{counter}</h1>
+// 				<button onClick={() => setCounter(counter + 1)}> increment</button>
+// 				<button onClick={() => setCounter(counter - 1)}> decrement</button>
+// 			</header>
+// 		</div>
+// 	);
+// }
+
+//CLASS
+
+//opción 1
+// sin este interface tira error al usar this.state.counter => dice que no existe en readonly{}
+// interface AppState {
+// 	counter: number;
+// }
+// class App extends Component<AppProps, AppState> {
+//opción 1
+
+//opción 2
+class App extends Component<AppProps> {
+	state = {
+		counter: 0,
+		title: this.props.title,
+	};
+	//opción 2
+	constructor(props: AppProps) {
+		super(props);
+		this.state = { title: props.title, counter: 0 };
+	}
+	onIncrement = (): void => {
+		this.setState({ counter: this.state.counter + 1 });
+	};
+	onDecrement = (): void => {
+		this.setState({ counter: this.state.counter - 1 });
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h5> Workshop {this.state.title} </h5>
+					<h1>{this.state.counter}</h1>
+					<button onClick={this.onIncrement}>Increment</button>
+					<button onClick={this.onDecrement}> Decrement</button>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
